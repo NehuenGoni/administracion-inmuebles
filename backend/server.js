15f512b666a61d39904f5a5ab00655d6d1a47975
Nehuen Goni <nehuenemilianogoni@gmail.com>
2024-11-06 18:16:31 -0300

    Connect frontend and backend using axios

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const tenants = require('./routes/tenants')
+const cors = require('cors')
 
 const app = express();
 const port = 3001; // Puerto en el que el servidor escuchará
+
+// Habilita CORS para todas las solicitudes
+app.use(cors());
 
 // Conexión a MongoDB
 mongoose.connect('mongodb+srv://nehuen_goni:K1HcUR7zsTYbccE0@tenantapp.zjj8b.mongodb.net/tenantApp?retryWrites=true&w=majority&appName=tenantApp', {
